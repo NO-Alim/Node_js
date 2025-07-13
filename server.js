fb218b4e74fa6459c8459c6f3253a670a347bb70
NO-Alim <abdulalimrakib53@gmail.com>
2025-07-13 23:36:32 +0600

    Task: Task Magager API

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-const app = require('./app');
-const { dbConnect } = require('./config/db');
-require('dotenv');
 
-const port = process.env.PORT || 3000;
+const express = require('express');
+const app = express();
 
+const PORT = process.env.PORT || 3000;
 
-dbConnect().then(() => {
-    app.listen(port, () => {
-        console.log(`Server is running on port ${port}`);
-    });
-}).catch((error) => {
-    console.error('Error connecting to MongoDB:', error);
+app.get('/', (req, res) => {
+  res.send('Server is running!');
 });
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
